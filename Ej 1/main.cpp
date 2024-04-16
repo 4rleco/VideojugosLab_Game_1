ec34421852b5b0b5bb32982164851d0df086b383
4rleco <hljoaco@gmail.com>
2024-04-16 16:22:18 -0300

    Add movement

--- Ej 1/main.cpp
+++ Ej 1/main.cpp
@@ -1,24 +1,57 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/System/Clock.hpp>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	Clock clock;
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	Time dt;
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	float rectangleX = 120.0f;	
+	float rectangleY = 20.0f;	
 
-    return 0;
+	RenderWindow window(VideoMode(1080, 720), "SFML works!");
+
+	RectangleShape rectangle;
+	rectangle.setSize(Vector2f(100, 50));
+	rectangle.setOutlineColor(Color::Red);
+	rectangle.setOutlineThickness(5);
+	rectangle.setPosition(rectangleX, rectangleY);
+
+	while (window.isOpen())
+	{
+		dt = clock.restart();
+
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::D))
+		{			
+			rectangleX += 100.0f * dt.asSeconds();			
+			rectangle.setPosition(rectangleX, rectangleY);
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Space))
+		{
+			rectangleY -= 50.0f * dt.asSeconds();			
+			rectangle.setPosition(rectangleX, rectangleY);
+		}
+
+		window.clear();
+		window.draw(rectangle);
+		window.display();
+	}
+
+	return 0;
 }
