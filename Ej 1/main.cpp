7c729bda17a95ad7518cabdbc5dbf00415407c89
4rleco <117652001+4rleco@users.noreply.github.com>
2024-04-23 15:05:28 -0300

    Add jump

--- Ej 1/main.cpp
+++ Ej 1/main.cpp
@@ -14,22 +14,45 @@ int main()
 
 	Time dt;
 
-	float rectangleX = 120.0f;	
-	float rectangleY = 20.0f;	
+	int screenWidth = 1080;
+	int screenHeight = 720;
 
-	RenderWindow window(VideoMode(1080, 720), "SFML works!");
+	float fwidth = screenWidth;
+	float fheight = screenHeight;
+	float FposX = 0.0f;
+	float FposY = 680.0f;
+
+	float initialPosX = 0.0f;	
+	float posX = initialPosX;	
+	float initialPosY = fheight - 70;	
+	float posY = initialPosY;	
+
+	float gravity = 10.0f;
+
+	Vector2f speed(100.0f, 50.0f);
+
+	bool isOnFloor = true;
+
+	RenderWindow window(VideoMode(screenWidth, screenHeight), "SFML works!");
 
 	RectangleShape rectangle;
 	rectangle.setSize(Vector2f(100, 50));
-	rectangle.setOutlineColor(Color::Red);
-	rectangle.setOutlineThickness(5);
-	rectangle.setPosition(rectangleX, rectangleY);
+	rectangle.setOutlineColor(Color::Blue);
+	rectangle.setOutlineThickness(10);
+	rectangle.setPosition(initialPosX, initialPosY);	
+
+	RectangleShape floor;
+	floor.setSize(Vector2f(fwidth,fheight));
+	floor.setOutlineColor(Color::Red);
+	floor.setOutlineThickness(5);
+	floor.setPosition(FposX, FposY);
 
 	while (window.isOpen())
 	{
 		dt = clock.restart();
 
 		Event event;
+
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed)
@@ -38,17 +61,38 @@ int main()
 
 		if (Keyboard::isKeyPressed(Keyboard::D))
 		{			
-			rectangleX += 100.0f * dt.asSeconds();			
-			rectangle.setPosition(rectangleX, rectangleY);
+			posX += speed.x * dt.asSeconds();
+			rectangle.setPosition(posX, posY);
+		}
+
+		if(Keyboard::isKeyPressed(Keyboard::A))
+		{			
+			posX -= speed.x * dt.asSeconds();
+			rectangle.setPosition(posX, posY);
 		}
 
 		if (Keyboard::isKeyPressed(Keyboard::Space))
 		{
-			rectangleY -= 50.0f * dt.asSeconds();			
-			rectangle.setPosition(rectangleX, rectangleY);
+			posY -= speed.y * dt.asSeconds();
+			rectangle.setPosition(posX, posY);
+			isOnFloor = false;
+		}
+
+		if (isOnFloor == false)
+		{
+			posY += gravity * dt.asSeconds();	
+
+			rectangle.setPosition(posX, posY);
+
+			if (posY >= initialPosY)
+			{
+				isOnFloor = true;
+				cout << "true" << endl;
+			}			
 		}
 
 		window.clear();
+		window.draw(floor);
 		window.draw(rectangle);
 		window.display();
 	}
